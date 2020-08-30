a5a785404944e4476aa2a858cf6adcc3e8f72850
SilvaBuenoVitor <vitot770@gmail.com>
2020-08-30 05:26:51 -0300

    Images added, text size and positioning treated, desktop version updated

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -8,7 +8,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 // Option 2
@@ -18,5 +18,22 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
+
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop;
+  var tags = $(".tag");
+  
+  for (var i = 0; i < tags.length; i++) {
+  var tag = tags[i];
+  
+  if ($(tag).position().top < pageBottom) {
+  $(tag).addClass("visible");
+  } else {
+  $(tag).removeClass("visible");
+  }
+  }
+  });
